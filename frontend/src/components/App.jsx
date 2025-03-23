f0079e6a8ffb7d0e8efad695f3d5092773e80176
mshraky3 <93828510+mshraky3@users.noreply.github.com>
2025-03-23 03:07:35 +0300

    Add files via upload

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -6,29 +6,29 @@ import CreateArea from "./CreateArea";
 import axios from "axios"
 
 
-
+const endpoint = "https://keeper-server-nine.vercel.app/"
 
 
 function App() {
 
   const [note_list, set_list] = useState([])
- 
+
   async function get_data() {
     try {
-      const res = await axios.get("http://localhost:3000")
+      const res = await axios.get(endpoint)
       console.log(res.data)
       set_list(res.data)
-    }catch(err){
+    } catch (err) {
       console.error("err")
     }
 
   }
 
   async function postData(data) {
-      const res = await axios.post("http://localhost:3000" , data ) ;
+    const res = await axios.post(endpoint, data);
   }
   async function deleteData(data) {
-    const res = await axios.post("http://localhost:3000/delete" , {data} ) ;
+    const res = await axios.post(endpoint + "delete", { data });
   }
 
   useEffect(() => {
@@ -37,7 +37,7 @@ function App() {
 
   async function add_note(event) {
     event.preventDefault();
-    const newNote = { title: event.target.title.value, content: event.target.content.value, id:( note_list.length + 1) } ;
+    const newNote = { title: event.target.title.value, content: event.target.content.value, id: (note_list.length + 1) };
     set_list([...note_list, newNote])
     postData(newNote);
     event.target.reset()
@@ -54,7 +54,7 @@ function App() {
       <Header />
       <CreateArea add_note={add_note} />
       {note_list.map((item, index) => {
-        return ( item.title.length >1 ? <Note key={index} id={item.id} title={item.title} content={item.content} delete_note={delete_note} /> : null )
+        return (item.title.length > 1 ? <Note key={index} id={item.id} title={item.title} content={item.content} delete_note={delete_note} /> : null)
       })}
       <Footer />
     </div>
